e65f39bf24b874b37c37ffe401e4d166debf79b9
Juan_Victor <estudantejuan0@gmail.com>
2023-01-27 19:35:42 -0300

    Listagem e criação de usuário

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router} from 'express';
 
 import { CreateUserController } from './controllers/user/createUserController';
+import { ListUsersController } from './controllers/user/listUsersController';
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle);
+router.get('/listusers', new ListUsersController().handle);
 
 export { router };
