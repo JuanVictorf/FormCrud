b211c4916aac7b875830db8ed5228b0985237b7e
Juan_Victor <estudantejuan0@gmail.com>
2023-01-28 14:02:34 -0300

    Cadastrando usuario no banco pelo front

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,43 @@
-import { useState } from 'react';
+import { useState, ChangeEvent, FormEvent} from 'react';
 import Head from 'next/head';
 import styles from '../../styles/home.module.scss';
+import { toast } from 'react-toastify';
 
+import { setupAPIClient } from '../services/api';
 
 export default function Home() {
   
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [numero, setNumero] = useState('');
+  const [cidade, setCidade] = useState('');
   
+
+  async function handleRegister(event: FormEvent) {
+    event.preventDefault();
+
+    if(name === '' || email === '' || numero === '' || cidade === ''){
+      toast.warning("Preencher o formulário corretamente.")
+      return;
+    }
+
+    const apiClient = setupAPIClient();
+
+    await apiClient.post('/users', {
+      name: name,
+      email: email,
+      numero: numero,
+      cidade: cidade
+    })
+
+    toast.success("Usuário criado com sucesso!");
+
+    setName('');
+    setEmail('');
+    setNumero('');
+    setCidade('');
+
+  }
 
   return (
     <>
@@ -16,11 +46,23 @@ export default function Home() {
     </Head>
     <div className={styles.cadastro}>
       <h1>Cadastro de usuário</h1>
-      <form>
-        <input type="text" className={styles.input} placeholder="Digite o seu Nome"/>
-        <input type="text" className={styles.input} placeholder="Digite o seu Email"/>
-        <input type="text" className={styles.input} placeholder="Digite o seu Numero"/>
-        <input type="text" className={styles.input} placeholder="Digite a sua Cidade"/>
+      <form onSubmit={handleRegister}>
+        <input type="text" className={styles.input} placeholder="Digite o seu Nome"
+        value={name}
+        onChange={ (e) => setName(e.target.value)}
+        />
+        <input type="text" className={styles.input} placeholder="Digite o seu Email"
+        value={email}
+        onChange={ (e) => setEmail(e.target.value)}
+        />
+        <input type="text" className={styles.input} placeholder="Digite o seu Numero"
+        value={numero}
+        onChange={ (e) => setNumero(e.target.value)}
+        />
+        <input type="text" className={styles.input} placeholder="Digite a sua Cidade"
+        value={cidade}
+        onChange={ (e) => setCidade(e.target.value)}
+        />
         <button className={styles.button} type="submit"> 
           <a>Cadastrar</a>
         </button>
